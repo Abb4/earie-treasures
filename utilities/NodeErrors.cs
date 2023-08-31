60abcf6859ec8b3c5e73273a1d860ff9a29283af
abby.abb <abb.coding@proton.me>
2023-08-31 14:46:00 +0200

    implemented result based extensions for common godot functions (#87)

---
+++ utilities/NodeErrors.cs
@@ -0,0 +1,116 @@
+using System;
+using CSharpFunctionalExtensions;
+using Godot;
+
+namespace utilities;
+
+public class NodeError : Exception, ICombine
+{
+	public NodeError() : base()
+	{
+	}
+
+	public NodeError(string message) : base(message)
+	{
+	}
+
+	public NodeError(string message, Exception innerException) : base(message, innerException)
+	{
+	}
+
+	public static NodeError AttemptedToExecuteOnNullNode<T>(string argumentName)
+	{
+		return new NodeError($"Attempted to call a function on null-node argument {argumentName}");
+	}
+
+	public static NodeError NodeIsNull<T>(Maybe<string> argumentName)
+	{
+		if(argumentName.HasValue)
+		{
+			return new NodeError($"{argumentName} is null when it should not be");
+		}
+		else
+		{
+			return new NodeError($"Node is null when it should not be");
+		}
+	}
+
+	public static NodeError PropertyIsNotSetUsingEditor<T>(string argumentName)
+	{
+        return new NodeError($"Node {argumentName} of type {typeof(T).Name} was not set using editor even though it should have been");
+	}
+
+	public ICombine Combine(ICombine value)
+	{
+		var ohter = value as NodeError;
+
+		var exceptions = new Exception[]
+		{
+			this,
+			ohter
+		};
+
+		return new NodeError("Multiple Errors Ocurred", new AggregateException(exceptions));
+	}
+}
+
+public class NodeSearchError : NodeError
+{
+	public NodeSearchError() : base()
+	{
+	}
+
+	public NodeSearchError(string message) : base(message)
+	{
+	}
+
+	public NodeSearchError(string message, Exception innerException) : base(message, innerException)
+	{
+	}
+
+	public static NodeError CouldNotFindNodeWhenSearching<T>()
+	{
+		return new NodeSearchError($"Could not locate the node of type {typeof(T).Name}");
+	}
+}
+
+public class NoParentError : NodeSearchError
+{
+	public NoParentError() : base()
+	{
+	}
+
+	public NoParentError(string message) : base(message)
+	{
+	}
+
+	public NoParentError(string message, Exception innerException) : base(message, innerException)
+	{
+	}
+
+	public static NodeError CouldNotLocateParent(Node node)
+	{
+		return new NoParentError($"Could not locate parent of {node.GetPath()}");
+	}
+}
+
+public class CouldNotCastToRequiredTypeError : NodeSearchError
+{
+	public CouldNotCastToRequiredTypeError() : base()
+	{
+	}
+
+	public CouldNotCastToRequiredTypeError(string message) : base(message)
+	{
+	}
+
+	public CouldNotCastToRequiredTypeError(string message, Exception innerException) : base(message, innerException)
+	{
+	}
+
+	public static NodeError CouldNotCastTo<T>(Node node)
+	{
+		return new CouldNotCastToRequiredTypeError($"Could not cast found node {node.GetPath()} " +
+				$"of type {node.GetType()} to requested type {typeof(T).Name}");
+	}
+}
--- utilities/NodeExtensions.cs
+++ utilities/NodeExtensions.cs
@@ -1,114 +1,214 @@
+using CSharpFunctionalExtensions;
 using Godot;
 using System;
 
 namespace utilities;
 
-public static class NodeExtensions {
+public static partial class NodeExtensions
+{
+    public static Result<T, NodeError> GetSceneRoot<T>(this T node)
+        where T : Node
+    {
+        var nodeResult = node.ToNodeResult();
 
-    public static bool TryFindParentNode<T>(this Node node, out T parentNode)
-        where T : class
-    {
-        Node parent;
-        
-        if ((parent = node.GetParent()) != null)
-        {
-            if (parent is T)
-            {
-                parentNode = parent as T;
-                return true;
-            }
-            else
-            {
-                return parent.TryFindParentNode(out parentNode);
-            }
-        }
-        else 
-        {
-            parentNode = null;
-            return false;
-        }
-    }
-    
-    public static T FindParentNodeIfNotSet<T>(this Node node, T value)
-        where T : class
-    {
-        if (value is not null)
-        {
-            return value;
-        }
-        else
-        {
-            if(node.TryFindParentNode<T>(out var parentValue))
-            {
-                return parentValue;
-            }
-            else
-            {
-                throw new ArgumentException($"Could not find a suitable parent class {typeof(T).Name} for node {node.GetPath()}");
-            }
-        }
+        return nodeResult.GetSceneRoot();
     }
 
-    public static Node GetSceneRoot(this Node node)
+    public static Result<T, NodeError> GetSceneRoot<T>(this Result<T, NodeError> node)
+        where T : Node
     {
-        return node.GetTree().Root.GetChild(0);
+        var rootWindow = node.Bind(n => n.GetTree().Root.ToNodeResult());
+
+        var sceneRoot = rootWindow.Bind(sr => sr.GetChild<T>(0).ToNodeResult());
+
+        return sceneRoot;
     }
 
-    public static bool TryFindNodeInChildrenRecursively<T>(this Node node, out T value)
-        where T : class
+    public static Result<TResult, NodeError> FindNodeInChildrenRecursively<TResult>(this Node parent)
+        where TResult : Node
     {
-        return node.TryFindNodeInChildrenRecursively(out value, (_) => true);
+        var nodeResult = parent.ToNodeResult();
+
+        return nodeResult.FindNodeInChildrenRecursively<TResult>((_) => true);
     }
 
-    public static bool TryFindNodeInChildrenRecursively<T>(this Node node, out T value, Func<T, bool> filter)
-        where T : class
+
+    public static Result<TResult, NodeError> FindNodeInChildrenRecursively<TResult>(this Result<Node, NodeError> parent)
+        where TResult : Node
     {
-        if(node is T && filter(node as T))
+        return parent.FindNodeInChildrenRecursively<TResult>((_) => true);
+    }
+
+    public static Result<TResult, NodeError> FindNodeInChildrenRecursively<TResult>(this Result<Node, NodeError> parent, Func<TResult, bool> filter)
+        where TResult : Node
+    {
+        if(parent.IsFailure)
         {
-            value = node as T;
-            return true;
+            return Result.Failure<TResult, NodeError>(parent.Error);
         }
 
-        foreach (var child in node.GetChildren())
+        if(parent.Value is TResult && filter(parent.Value as TResult))
         {
-            if(child.TryFindNodeInChildrenRecursively(out value, filter))
+            return Result.Success<TResult, NodeError>(parent.Value as TResult);
+        }
+
+        foreach (var child in parent.Value.GetChildren())
+        {
+            var childResult = child.ToNodeResult().FindNodeInChildrenRecursively(filter);
+
+            if(childResult.IsSuccess)
             {
-                return true;
+                return childResult;
             }
         }
-        
-        value = null;
-        return false;
-    } 
 
-    public static bool TryFindNodeInScene<T>(this Node node, out T value)
-        where T : class
-    { 
-        var root = node.GetSceneRoot();
-        
-        return root.TryFindNodeInChildrenRecursively(out value);
+        return NodeSearchError.CouldNotFindNodeWhenSearching<TResult>();
     }
 
-    public static bool TryFindNodeInScene<T>(this Node node, out T value, Func<T, bool> filter)
-        where T : class
-    { 
-        var root = node.GetSceneRoot();
-        
-        return root.TryFindNodeInChildrenRecursively(out value, filter);
-    }
-    
-    public static void MoveToParent(this Node node, Node target)
+    public static Result<T, NodeError> TryFindNodeInScene<T>(this Node node)
+        where T : Node
     {
-        var sourceTarget = node.GetParent();
-        sourceTarget.RemoveChild(node);
-        target.AddChild(node);
+        var root = node.ToNodeResult().GetSceneRoot();
+
+        return root.TryFindNodeInScene<T>();
     }
-    
-    public static void ResetRotation<T>(this T node)
+
+    public static Result<T, NodeError> TryFindNodeInScene<T>(this Result<Node, NodeError> node)
+        where T : Node
+    {
+        return node.FindNodeInChildrenRecursively<T>();
+    }
+
+    public static UnitResult<NodeError> MoveToParent(this Node node, Node target)
+    {
+        var targetResult = target.ToNodeResult(nameof(target));
+
+        if (targetResult.IsFailure)
+        {
+            return targetResult;
+        }
+
+        var nodeResult = node.ToNodeResult(nameof(node));
+        var nodeParent = nodeResult.Bind(n => n.ToNodeResult().GetParentSafe<Node>());
+
+        if (nodeParent.IsFailure)
+        {
+            return nodeParent;
+        }
+
+        nodeParent.Value.RemoveChild(nodeResult.Value);
+        targetResult.Value.AddChild(nodeResult.Value);
+
+        return UnitResult.Success<NodeError>();
+    }
+
+    public static UnitResult<NodeError> ResetRotation<T>(this T node)
         where T : Node3D
     {
-        Transform3D transform = node.Transform;
-        transform.Basis = Basis.Identity;
-        node.Transform = transform;
+        var nodeResult = node.ToNodeResult(nameof(node));
+
+        return ResetRotation(nodeResult);
+    }
+
+    private static UnitResult<NodeError> ResetRotation<T>(Result<T, NodeError> nodeResult)
+        where T : Node3D
+    {
+        var transformResult = nodeResult.Map(n => n.Transform);
+
+        if (transformResult.TryGetValue(out var transform))
+        {
+            transform.Basis = Basis.Identity;
+            nodeResult.Value.Transform = transform;
+        }
+
+        return transformResult;
+    }
+
+    public static Result<TParent, NodeError> GetParentSafe<TParent>(this Node node)
+        where TParent : Node
+    {
+        return node.ToNodeResult().GetParentSafe<TParent>();
+    }
+
+    public static Result<TParent, NodeError> GetParentSafe<TParent>(this Maybe<Node> node)
+        where TParent : Node
+    {
+        return node.ToNodeResult().GetParentSafe<TParent>();
+    }
+
+    public static Result<TParent, NodeError> GetParentSafe<TParent>(this Result<Node, NodeError> nodeResult)
+        where TParent : Node
+    {
+        var parent = nodeResult.Bind(n =>
+            n.GetParent().ToResult(NoParentError.CouldNotLocateParent(n)));
+
+        var castParent = parent.Bind(p =>
+            (p as TParent).ToResult(CouldNotCastToRequiredTypeError.CouldNotCastTo<Node>(p)));
+
+        return castParent;
+    }
+
+    public static Result<T, NodeError> CompensateFromChildren<T>(this Result<T, NodeError> nodeResult, Node parent)
+        where T : Node
+    {
+        if(nodeResult.IsFailure)
+        {
+            return parent.FindNodeInChildrenRecursively<T>();
+        }
+
+        return nodeResult;
+    }
+
+    public static Result<T, NodeError> CompensateFromParent<T>(this T node, Node child)
+        where T : Node
+    {
+        var nodeResult = node.ToNodeResult();
+
+        return nodeResult.CompensateFromParent(child);
+    }
+
+    public static Result<T, NodeError> CompensateFromParent<T>(this Result<T, NodeError> nodeResult, Node child)
+        where T : Node
+    {
+        if(nodeResult.IsFailure)
+        {
+            return child.ToNodeResult().GetParentSafe<T>();
+        }
+
+        return nodeResult;
+    }
+}
+
+public static class ResultExtensions
+{
+    public static Result<T, NodeError> ToNodeResult<T>(this T node, Maybe<string> attributeName = default)
+    {
+        return Maybe.From(node).ToNodeResult(attributeName);
+    }
+
+    public static Result<T, NodeError> ToNodeResult<T>(this Maybe<T> node, Maybe<string> attributeName = default)
+    {
+        return node.ToResult(error: NodeError.NodeIsNull<Node>(attributeName));
+    }
+
+    public static T AssertEditorPropertySet<T>(this T property, string attributeName)
+    {
+        var maybe = Maybe.From(property);
+
+        return maybe.AssertEditorPropertySet(attributeName);
+    }
+
+    public static T AssertEditorPropertySet<T>(this Maybe<T> property, string attributeName)
+    {
+        return property
+            .ToResult(error: NodeError.PropertyIsNotSetUsingEditor<T>(attributeName))
+            .ThrowIfValueNotSet();
+    }
+
+    public static T ThrowIfValueNotSet<T, E>(this Result<T, E> result)
+    {
+        var value = result.Value;
+
+        return value;
     }
 }
